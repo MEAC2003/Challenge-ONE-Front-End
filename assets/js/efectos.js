29b25dfc0bdb2fbdb110e01f943688ebd99d354d
MEAC <manuelantoncisneros2020@hotmail.com>
2023-03-28 01:35:28 -0500

    Finalizó la implementación de la funcionalidad de responsividad y se completó la sección "Acerca de mí" y la sección "Hobbies" con la información correspondiente.

--- assets/js/efectos.js
+++ assets/js/efectos.js
@@ -29,39 +29,6 @@ let typingText = new Typed("#text", {
   backSpeed: 50,
   backDelay: 1000,
 });
-
-const texto = document.querySelector('.sobre__info');
-texto.innerHTML = texto.textContent.replace(/\S/g, "<span>$&</span>");
-
-const animacion = anime.timeline({
-  targets: '.sobre__info span',
-  easing: 'easeInOutExpo',
-  loop: false,
-});
-
-animacion
-  .add({
-    rotate: function () {
-      return anime.random(-360, 360)
-    },
-    translateX: function () {
-      return anime.random(-35, 35)
-    },
-    translateY: function () {
-      return anime.random(-35, 35)
-    },
-    duration: 300,
-    delay: anime.stagger(5),
-  })
-
-
-  .add({
-    rotate: 0,
-    translateX: 0,
-    translateY: 0,
-    delay: anime.stagger(9),
-  });
-
 window.addEventListener('scroll', function () {
   const sections = document.querySelectorAll('.section');
   const navLinks = document.querySelectorAll('.nav-link');
